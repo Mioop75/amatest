90296005efa53ef637c418b3dbccc87cb2b3d4f5
Matvey <83799026+Mioop75@users.noreply.github.com>
2024-06-12 23:35:15 +0700

    Add files via upload

--- client/src/components/Table.vue
+++ client/src/components/Table.vue
@@ -1,5 +1,5 @@
 <template>
-	<div v-if="loading">Loading...</div>
+	<a-input placeholder="От трех символов" :value="search" @change="searchItem($event)" />
 	<a-table :columns="columns" :data-source="items">
 		<template #bodyCell="{ column, record }">
 			<template v-if="column.key === 'name'">
@@ -32,7 +32,6 @@ import dayjs from 'dayjs'
 export default {
 	setup() {
 		return {
-			loading: false,
 			columns: [
 				{
 					title: 'Название',
@@ -64,22 +63,30 @@ export default {
 	},
 	data() {
 		return {
+			search: '',
 			items: [] as any[],
 		}
 	},
 	created() {
 		this.fetchData()
 	},
-	methods: {
-		async fetchData() {
 
-			const response = await fetch('http://localhost:3000/api/orders')
+	methods: {
+		async fetchData(search?: string) {
+
+			const response = await fetch(`http://localhost:3000/api/orders?${search ? 'name=' + search : ''}`)
 			const data = await response.json()
 			const leads = data.map((lead: any) => ({
 				...lead,
 				created_at: dayjs(lead.created_at).format('DD/MM/YYYY H:HH a')
 			}))
 			this.items = leads
+		},
+		searchItem: function (event: any) {
+			setTimeout(() => {
+				this.search = event.target.value
+				this.fetchData(event.target.value)
+			}, 1000)
 		}
 	},
 }
